e944ed62fbb2ec495f1f27135211ad0b43130c46
Energyshifter8 <temuulenbold1@gmail.com>
2026-07-07 15:25:12 +0800

    spida

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -6,11 +6,7 @@ export default function Home() {
   return (
     <div className="relative min-h-screen bg-zinc-950">
       <SpiderNet />
-      <div className="relative z-10 flex min-h-screen items-center justify-center">
-        <h1 className="text-6xl font-bold text-white tracking-tight">
-          Spider Net
-        </h1>
-      </div>
+
     </div>
   );
 }
